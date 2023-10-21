5dd2558161c372a47638ca525a503d666d07d3a9
ounajib <ounajib@ada.local.isima.fr>
2023-10-21 16:13:40 +0200

    Adding quality less then 50  test

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -22,6 +22,38 @@ class GildedRoseTest {
     app.updateQuality();
     assertEquals(9, element.quality, "quality should be 9");
   }
+  @Test
+  @DisplayName("Test any product after one day sellin decrease")
+  void afterOneDaySellinDecrease() {
+    Item element = new Item("ANY1", 5, 10);
+    GildedRose app = new GildedRose(new Item[] {element});
+    app.updateQuality();
+    assertEquals(element.sellIn, 4,"Sellin should be 4");
+  }
+  @Test
+  @DisplayName("Test Aged Brie product after one day quality increase")
+  void afterOneDayAgedBrieQualityIncrease() {
+    Item element = new Item("Aged Brie", 5, 10);
+    GildedRose app = new GildedRose(new Item[] {element});
+    app.updateQuality();
+    assertEquals(element.quality, 11,"Quality should be 11");
+  }
+  
+  @Test
+  @DisplayName("Test Backstage passes to a TAFKAL80ETC concert product after last day goes to zero")
+  void afterLastDayBackstagePassesToaTAFKAL80ETCconcertGoesToZero() {
+    Item element = new Item("Backstage passes to a TAFKAL80ETC concert", 0, 10);
+    GildedRose app = new GildedRose(new Item[] {element}); 
+    app.updateQuality();
+    assertEquals(element.quality, 0, "quality goes to 0");
+  }
 
-
+  @Test
+  @DisplayName("The quality of any product will never exceed 50")
+  void theQualityOfAnyProductWillNeverExceedFifty() {
+    Item element = new Item("ANY2", 5, 50);
+    GildedRose app = new GildedRose(new Item[] {element});
+    app.updateQuality();
+    assertTrue(element.quality <= 50,"Quality will never exceed 50");
+  }
 }
